ffce48eb8cac3a383562ed096cda3fc3f9c2df74
erencanar <02185076006@ogr.inonu.edu.tr>
2022-12-04 22:49:02 +0300

    kelimebul.py dosyasına kod eklendi!

---
+++ kelimebul.py
@@ -0,0 +1,16 @@
+metin = """Bu programlama dili Guido Van Rossum adlı Hollandalı bir programcı
+tarafından 90’lı yılların başında geliştirilmeye başlanmıştır. Çoğu insan,
+isminin Python olmasına aldanarak, bu programlama dilinin, adını piton
+yılanından aldığını düşünür. Ancak zannedildiğinin aksine bu programlama dilinin
+adı piton yılanından gelmez. Guido Van Rossum bu programlama dilini, The Monty
+Python adlı bir İngiliz komedi grubunun, Monty Python’s Flying Circus adlı
+gösterisinden esinlenerek adlandırmıştır. Ancak her ne kadar gerçek böyle olsa
+da, Python programlama dilinin pek çok yerde bir yılan figürü ile temsil
+edilmesi neredeyse bir gelenek halini almıştır."""
+
+aranan_harf=input("Lütfen aradığınız harfi giriniz:\t")
+adet=0
+for karakter in metin:
+    if karakter==aranan_harf:
+        adet+=1
+print(adet)

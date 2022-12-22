7fe110f4c58d0004360ae015ca93c7aa0e68af16
cll <61542865+CelalBinici7@users.noreply.github.com>
2022-12-22 14:56:37 +0300

    Hasar alma ve tuzak fonksiyonları eklendi.

--- Saglık.cs
+++ Saglık.cs
@@ -21,7 +21,36 @@ public class Health : MonoBehaviour {
         spriteRend = GetComponent<SpriteRenderer>();
     }
 
-  
+      public void TakeDamage(float _damage) {
+        currentHealt = Mathf.Clamp(currentHealt - _damage, 0, startingHealth);
+
+        if (currentHealt > 0)
+        {
+            anima.SetTrigger("hurt");
+            StartCoroutine(Invunerability());
+        }
+        else
+        {
+            if (!dead)
+            {
+                anima.SetTrigger("die");
+                GetComponent<PlayerMovment>().enabled = false;
+                dead = true;
+            }
+        }
+    }
+
+    private IEnumerator Invunerability() {
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
+    }
 
 
 
